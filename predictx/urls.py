14b15c1da8e409ba2a093d6ba999c498be4d4e72
joshdasilva <altondasilva@live.com>
2019-04-03 16:47:51 +0100

    navigation

--- predictx/urls.py
+++ predictx/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
 app_name = "predictx"
@@ -6,9 +7,74 @@ app_name = "predictx"
 urlpatterns = [
   #  path('', views.index),
     path("", views.indexpage, name="indexpage"),
-    path("dashboard/", views.dashboard, name="dashboard"),
     path("register/", views.register, name="register"),
     path("login/", views.login_request, name="login"),
-    path("dashboard/logout", views.logout_request, name="logout"),
+
+    path("AAPL/", views.AAPL, name="AAPL"),
+    path("MSFT/", views.MSFT, name="MSFT"),
+    path("AMZN/", views.AMZN, name="AMZN"),
+    path("FB/", views.FB, name="FB"),
+    path("BRK.B/", views.BRKB, name="BRK.B"),
+    path("GOOG/", views.GOOG, name="GOOG"),
+    path("XOM/", views.XOM, name="XOM"),
+    path("JPM/", views.JPM, name="JPM"),
+    path("V/", views.V, name="V"),
+    path("BAC/", views.BAC, name="BAC"),
+
+    path("INTC/", views.INTC, name="INTC"),
+    path("CSCO/", views.CSCO, name="CSCO"),
+    path("VZ/", views.VZ, name="VZ"),
+    path("PFE/", views.PFE, name="PFE"),
+    path("T/", views.T, name="T"),
+    path("MA/", views.MA, name="MA"),
+    path("DIS/", views.DIS, name="DIS"),
+    path("KO/", views.KO, name="KO"),
+    path("PEP/", views.PEP, name="PEP"),
+    path("NFLX/", views.NFLX, name="NFLX"),
+
+    path("MCD/", views.MCD, name="MCD"),
+    path("WMT/", views.WMT, name="WMT"),
+    path("ORCL/", views.ORCL, name="ORCL"),
+    path("IBM/", views.IBM, name="IBM"),
+    path("PYPL/", views.PYPL, name="PYPL"),
+    path("NVDA/", views.NVDA, name="NVDA"),
+    path("NKE/", views.NKE, name="NKE"),
+    path("COST/", views.COST, name="COST"),
+    path("QCOM/", views.QCOM, name="QCOM"),
+
+    path("AAPL/logout", views.logout_request, name="logout"),
+    path("MSFT/logout", views.logout_request, name="logout"),
+    path("AMZN/logout", views.logout_request, name="logout"),
+    path("FB/logout", views.logout_request, name="logout"),
+    path("BRK.B/logout", views.logout_request, name="logout"),
+    path("GOOG/logout", views.logout_request, name="logout"),
+    path("XOM/logout", views.logout_request, name="logout"),
+    path("JPM/logout", views.logout_request, name="logout"),
+    path("V/logout", views.logout_request, name="logout"),
+    path("BAC/logout", views.logout_request, name="logout"),
+    path("INTC/logout", views.logout_request, name="logout"),
+    path("CSCO/logout", views.logout_request, name="logout"),
+    path("VZ/logout", views.logout_request, name="logout"),
+    path("PFE/logout", views.logout_request, name="logout"),
+    path("T/logout", views.logout_request, name="logout"),
+    path("MA/logout", views.logout_request, name="logout"),
+    path("DIS/logout", views.logout_request, name="logout"),
+    path("KO/logout", views.logout_request, name="logout"),
+    path("PEP/logout", views.logout_request, name="logout"),
+    path("NFLX/logout", views.logout_request, name="logout"),
+    path("MCD/logout", views.logout_request, name="logout"),
+    path("WMT/logout", views.logout_request, name="logout"),
+    path("ORCL/logout", views.logout_request, name="logout"),
+    path("IBM/logout", views.logout_request, name="logout"),
+    path("PYPL/logout", views.logout_request, name="logout"),
+    path("NVDA/logout", views.logout_request, name="logout"),
+    path("NKE/logout", views.logout_request, name="logout"),
+    path("COST/logout", views.logout_request, name="logout"),
+    path("QCOM/logout", views.logout_request, name="logout"),
+
+
+
+
+
 ]
 
--- predictx/views.py
+++ predictx/views.py
@@ -21,13 +21,133 @@ def indexpage(request):
     return render(request = request,
                   template_name='predictx/index.html',
                   context = {"stocks":Stock.objects.all})
-def dashboard(request):
-    return render(request, 'predictx/dashboard.html')
+def AAPL(request):
+    return render(request, 'predictx/symbols/AAPL.html')
 
-def dashboard(request):
+def AAPL(request):
     return render(request = request,
-                  template_name='predictx/dashboard.html',
+                  template_name='predictx/symbols/AAPL.html',
                   context = {"stocks":Stock.objects.all})
+
+
+def MSFT(request):
+    return render(request = request,
+                  template_name='predictx/symbols/MSFT.html',
+                  context = {"stocks":Stock.objects.all})
+def AMZN(request):
+    return render(request = request,
+                  template_name='predictx/symbols/AMZN.html',
+                  context = {"stocks":Stock.objects.all})
+def FB(request):
+    return render(request = request,
+                  template_name='predictx/symbols/FB.html',
+                  context = {"stocks":Stock.objects.all})
+def BRKB(request):
+    return render(request = request,
+                  template_name='predictx/symbols/BRKB.html',
+                  context = {"stocks":Stock.objects.all})
+def GOOG(request):
+    return render(request = request,
+                  template_name='predictx/symbols/GOOG.html',
+                  context = {"stocks":Stock.objects.all})
+def XOM(request):
+    return render(request = request,
+                  template_name='predictx/symbols/XOM.html',
+                  context = {"stocks":Stock.objects.all})
+def JPM(request):
+    return render(request = request,
+                  template_name='predictx/symbols/JPM.html',
+                  context = {"stocks":Stock.objects.all})
+def V(request):
+    return render(request = request,
+                  template_name='predictx/symbols/V.html',
+                  context = {"stocks":Stock.objects.all})
+def BAC(request):
+    return render(request = request,
+                  template_name='predictx/symbols/BAC.html',
+                  context = {"stocks":Stock.objects.all})
+def INTC(request):
+    return render(request = request,
+                  template_name='predictx/symbols/INTC.html',
+                  context = {"stocks":Stock.objects.all})
+def CSCO(request):
+    return render(request = request,
+                  template_name='predictx/symbols/CSCO.html',
+                  context = {"stocks":Stock.objects.all})
+def VZ(request):
+    return render(request = request,
+                  template_name='predictx/symbols/VZ.html',
+                  context = {"stocks":Stock.objects.all})
+def PFE(request):
+    return render(request = request,
+                  template_name='predictx/symbols/PFE.html',
+                  context = {"stocks":Stock.objects.all})
+def T(request):
+    return render(request = request,
+                  template_name='predictx/symbols/T.html',
+                  context = {"stocks":Stock.objects.all})
+def MA(request):
+    return render(request = request,
+                  template_name='predictx/symbols/MA.html',
+                  context = {"stocks":Stock.objects.all})
+def DIS(request):
+    return render(request = request,
+                  template_name='predictx/symbols/DIS.html',
+                  context = {"stocks":Stock.objects.all})
+def KO(request):
+    return render(request = request,
+                  template_name='predictx/symbols/KO.html',
+                  context = {"stocks":Stock.objects.all})
+def PEP(request):
+    return render(request = request,
+                  template_name='predictx/symbols/PEP.html',
+                  context = {"stocks":Stock.objects.all})
+def NFLX(request):
+    return render(request = request,
+                  template_name='predictx/symbols/NFLX.html',
+                  context = {"stocks":Stock.objects.all})
+def MCD(request):
+    return render(request = request,
+                  template_name='predictx/symbols/MCD.html',
+                  context = {"stocks":Stock.objects.all})
+def WMT(request):
+    return render(request = request,
+                  template_name='predictx/symbols/WMT.html',
+                  context = {"stocks":Stock.objects.all})
+def ORCL(request):
+    return render(request = request,
+                  template_name='predictx/symbols/ORCL.html',
+                  context = {"stocks":Stock.objects.all})
+def IBM(request):
+    return render(request = request,
+                  template_name='predictx/symbols/IBM.html',
+                  context = {"stocks":Stock.objects.all})
+def PYPL(request):
+    return render(request = request,
+                  template_name='predictx/symbols/PYPL.html',
+                  context = {"stocks":Stock.objects.all})
+def NVDA(request):
+    return render(request = request,
+                  template_name='predictx/symbols/NVDA.html',
+                  context = {"stocks":Stock.objects.all})
+def NKE(request):
+    return render(request = request,
+                  template_name='predictx/symbols/NKE.html',
+                  context = {"stocks":Stock.objects.all})
+def COST(request):
+    return render(request = request,
+                  template_name='predictx/symbols/COST.html',
+                  context = {"stocks":Stock.objects.all})
+def QCOM(request):
+    return render(request = request,
+                  template_name='predictx/symbols/QCOM.html',
+                  context = {"stocks":Stock.objects.all})
+def MSFT(request):
+    return render(request = request,
+                  template_name='predictx/symbols/MSFT.html',
+                  context = {"stocks":Stock.objects.all})
+
+
 
 
 def logout_request(request):
@@ -45,7 +165,7 @@ def login_request(request):
             if user is not None:
                 login(request, user)
                 messages.info(request, f"You're now logged in as {username}")
-                return redirect('/dashboard')     #/dashboad
+                return redirect('/AAPL')     #/dashboad
             else:
                 messages.error(request, "Incorrect Username or Password")
         else:
